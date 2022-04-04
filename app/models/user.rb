45980d0938a29a590bff54458e9b4cb51ec50a93
haruka tezuka <th_kiyose@outlook.com>
2022-04-04 14:48:22 +0900

    Userバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :name, presence: true, length: {maximum: 30}
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, length: {maximum:255}
+  validates :password, presence: true, length: {minimum: 6}
 end
