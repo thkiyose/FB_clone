bbfca2599d3c38759bade60270d390e4cb05330f
haruka tezuka <th_kiyose@outlook.com>
2022-04-05 11:54:20 +0900

    ログインしている時に時分のマイページだけを表示できるよう分岐を記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_or_redirect, only: [:show]
   def new
     @user = User.new
   end
@@ -15,11 +16,22 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    if @user.id != current_user.id
+      redirect_to user_path(current_user.id)
+    end
   end
 
   private
 
   def user_params
     params.require(:user).permit(:name,:email,:password,:password_confirmation)
+  end
+
+  def current_user
+    current_user ||= User.find(session[:user_id])
+  end
+
+  def logged_in_or_redirect
+    redirect_to new_session_path unless current_user
   end
 end
