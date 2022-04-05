16b9e5258b695100aa71144c4476cf5b000fc4ea
haruka tezuka <th_kiyose@outlook.com>
2022-04-05 11:43:01 +0900

    user作成後にログインする処理を追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   validates :name, presence: true, length: { maximum: 30}
-  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, length: {maximum:255}
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, length: {maximum:255}, uniqueness: true
   validates :password, presence: true, length: {minimum: 6}
   before_validation { email.downcase! }
 end
