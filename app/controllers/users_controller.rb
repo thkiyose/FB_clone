59230b6585a60091b44e5bd03faf8a36c39f18f0
haruka tezuka <th_kiyose@outlook.com>
2022-04-05 12:08:37 +0900

    usersコントローラーの一部メソッドをsessionsヘルパーに移動

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,16 +27,4 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name,:email,:password,:password_confirmation)
   end
-
-  def current_user
-    current_user ||= User.find(session[:user_id])
-  end
-
-  def can_if_logged_in
-    redirect_to new_session_path unless current_user
-  end
-
-  def can_if_not_logged_in
-    redirect_to user_path(current_user.id) if current_user
-  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,13 @@
-module SessionsHelper
+module SessionsHelper  
+  def current_user
+    current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def can_if_logged_in
+    redirect_to new_session_path unless current_user
+  end
+
+  def can_if_not_logged_in
+    redirect_to user_path(current_user.id) if current_user
+  end
 end
