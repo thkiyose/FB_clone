b96c5ba962f23c5388ad6bb053694291c255b8d9
haruka tezuka <th_kiyose@outlook.com>
2022-04-05 12:23:07 +0900

    UserとPostのアソシエーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :posts
   validates :name, presence: true, length: { maximum: 30}
   validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, length: {maximum:255}, uniqueness: true
   validates :password, presence: true, length: {minimum: 6}
